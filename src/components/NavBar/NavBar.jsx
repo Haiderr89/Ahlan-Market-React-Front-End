9bace1d2b3017006e629a5036370024212c9fd8d
Hakim <hakimalhaddad2@gmail.com>
2025-03-03 14:02:38 +0300

    Display Designed

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,71 +2,75 @@ import { Link } from "react-router-dom";
 import { AuthedUserContext } from "../../App";
 import { useContext } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import '../../App.css';
+import "../../App.css";
 
 const NavBar = ({ handleSignout }) => {
-  const user = useContext(AuthedUserContext);
-  return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container-fluid cont">
-        <Link className="navbar-brand" to="/">
-          AhlanMarket
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            {user ? (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" to={`/market/new`}>
-                    Add Product
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={`/market`}>
-                    View Products
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/auth/sign-out" onClick={handleSignout}>
-                    Sign Out
-                  </Link>
-                </li>
-              </>
-            ) : (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/signup">
-                    Sign Up
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/signin">
-                    Sign In
-                  </Link>
-                </li>
-              </>
-            )}
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+	const user = useContext(AuthedUserContext);
+	return (
+		<nav className="navbar navbar-expand-lg">
+			<div className="container-fluid cont">
+				<Link className="navbar-brand" to="/">
+					AhlanMarket
+				</Link>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div id="navbarSupportedContent">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<Link className="nav-link" to="/">
+								Home
+							</Link>
+						</li>
+						{user ? (
+							<>
+								<li className="nav-item">
+									<Link className="nav-link" to={`/market/new`}>
+										Add Product
+									</Link>
+								</li>
+								<li className="nav-item">
+									<Link className="nav-link" to={`/market`}>
+										View Products
+									</Link>
+								</li>
+								<li className="nav-item">
+									<Link
+										className="nav-link"
+										to="/auth/sign-out"
+										onClick={handleSignout}
+									>
+										Sign Out
+									</Link>
+								</li>
+							</>
+						) : (
+							<>
+								<li className="nav-item">
+									<Link className="nav-link" to="/signup">
+										Sign Up
+									</Link>
+								</li>
+								<li className="nav-item">
+									<Link className="nav-link" to="/signin">
+										Sign In
+									</Link>
+								</li>
+							</>
+						)}
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default NavBar;
--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,18 +1,66 @@
 import { useContext } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import '../../App.css';
-import { Link } from 'react-router-dom';
-
+import "../../App.css";
+import { Link } from "react-router-dom";
 
 const ProductList = (props) => {
-    // console.log(props)
-    return (
-      <main>
-        {props.market.map((market) => (
-          <p key={market._id}>{market.name}</p>
-        ))}
-      </main>
-    );
-  };
+	return (
+		<main>
+			{props.market.map((market) => (
+				<div class="album py-5 bg-light">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-4">
+								<div class="card mb-4 shadow-sm">
+									<svg
+										class="bd-placeholder-img card-img-top"
+										width="100%"
+										height="225"
+										xmlns="http://www.w3.org/2000/svg"
+										preserveAspectRatio="xMidYMid slice"
+										focusable="false"
+										role="img"
+										aria-label="Placeholder: Thumbnail"
+									>
+										<title>Placeholder</title>
+										<rect width="100%" height="100%" fill="#55595c"></rect>
+										<text x="50%" y="50%" fill="#eceeef" dy=".3em">
+											Thumbnail
+										</text>
+									</svg>
+									<div class="card-body">
+										<p key={market._id}>
+											<h1>{market.name}</h1>
+											<h2>{market.price}</h2>
+											{market.category}
+										</p>
 
-export default ProductList
+										<div class="d-flex justify-content-between align-items-center">
+											<div class="btn-group">
+												<button
+													type="button"
+													class="btn btn-sm btn-outline-secondary"
+												>
+													View
+												</button>
+												<button
+													type="button"
+													class="btn btn-sm btn-outline-secondary"
+												>
+													Edit
+												</button>
+											</div>
+											<small class="text-muted">9 mins</small>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			))}
+		</main>
+	);
+};
+
+export default ProductList;
